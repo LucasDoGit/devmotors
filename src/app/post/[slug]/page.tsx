8717aca87bb02689538437829beff8e36ccbdfd6
lucastimoteo <95625902+LucasDoGit@users.noreply.github.com>
2025-01-14 01:14:37 -0300

    fix: params recebidos com promises

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -5,7 +5,7 @@ import { Metadata } from 'next';
 import { Content } from './components/content';
 import { LoadingPost } from './components/loading';
 
-export async function generateMetadata({ params }: { params: { slug: string }}): Promise<Metadata> {
+export async function generateMetadata({ params }: { params: Promise<{ slug: string }>}): Promise<Metadata> {
     const { slug } = await params;
 
     try {
@@ -47,7 +47,7 @@ export async function generateMetadata({ params }: { params: { slug: string }}):
     }
 }
 
-export default async function Page({ params }: { params: { slug: string } }) {
+export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
     const { slug } = await params;
 
     return (
